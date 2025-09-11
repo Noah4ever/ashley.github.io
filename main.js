0d6c1451272ced3103b5ebf068dce34aa18714f0
Lea Drewes <lea.drewes@hec.de>
2025-09-11 09:40:58 +0200

    feat Loving noah

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+let button = document.getElementById("toggleMode");
+
+ button.addEventListener("click", ()=>{
+    document.body.classList.toggle("dark");
+    document.body.classList.toggle("light");
+ });
+
 
