3c0e9f6158ad60502c12aacede609bca54d199fe
leaa321 <ashleyletsplay@gmail.com>
2025-09-22 17:36:36 +0200

    added animation to timeline

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ button.addEventListener("click", () => {
     ? "sunny"
     : "rainy";
 });
+
+const schools = document.querySelectorAll(".school");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show"); // sichtbar machen
+      } else {
+        entry.target.classList.remove("show"); // ausblenden beim Hochscrollen
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+schools.forEach((school) => {
+  observer.observe(school);
+});
